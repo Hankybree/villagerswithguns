f1f8b25349901581680b29d8fdfe9da31851a6b7
Henrik Frank <frank.havsvinge@gmail.com>
2021-04-07 15:43:49 +0200

    Initial commit

---
+++ src/main/java/se/hedonix/villagerswithguns/Main.java
@@ -0,0 +1,20 @@
+package se.hedonix.villagerswithguns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
